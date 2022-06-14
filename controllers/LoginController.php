b967cac37e0b1b113e998319a742a2761b4b96f9
Emmanuel Osnaya <92100726+Emosnaya@users.noreply.github.com>
2022-06-13 23:04:06 -0500

    Se  ajustó las variables de entrono

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -2,16 +2,49 @@
 
 namespace Controllers;
 
+use MVC\Router;
+
 class LoginController {
 
-    public static function login(){
+    public static function login(Router $router){
+        $alertas = [];
 
         if($_SERVER['REQUEST_METHOD'] === 'POST'){
 
         }
 
+        $router->render('auth/login', [
+
+        ]);
+
     } 
     public static function logout(){
-        
+
+    }
+    public static function crear(){
+
+
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+        }
     } 
+    public static function olvide(){
+        
+
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+        }
+    }  
+    public static function reestablecer(){
+
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+        }
+    } 
+    public static function mensaje(){
+
+    }
+    public static function confirmar(){
+        
+    }
 }
