47d45844cd872724d4fcad7ab3e6b9d54ed147d4
Emmanuel Osnaya <92100726+Emosnaya@users.noreply.github.com>
2022-06-14 21:08:56 -0500

    Se creo el modelo de Usuario

---
+++ models/Usuario.php
@@ -0,0 +1,42 @@
+<?php 
+
+namespace Model;
+
+class Usuario extends ActiveRecord {
+    protected static $tabla = 'usuarios';
+    protected static $columnasDB = ['id', 'nombre', 'email', 'password', 'token', 'confirmado'];
+
+    public function __construct($args = [])
+    {
+        $this->id = $args['id'] ?? null;
+        $this->nombre = $args['nombre'] ?? '';
+        $this->email = $args['email'] ?? '';
+        $this->password = $args['password'] ?? '';
+        $this->password2 = $args['password2'] ?? null;
+        $this->token = $args['token'] ?? '';
+        $this->confirmado = $args['confirmado'] ?? '';
+
+    }
+    //Validar el nombre
+    public function validarNuevaCuenta(){
+        if(!$this->nombre){
+            self::$alertas['error'][] = 'El nombre del usuario es obligatorio';
+        }
+        if(!$this->email){
+            self::$alertas['error'][] = 'El email del usuario es obligatorio';
+        }
+        if(!$this->password){
+            self::$alertas['error'][] = 'El password es obligatorio';
+        }
+        if(strlen($this->password) < 6 ){
+            self::$alertas['error'][] = 'El password debe tener al menos 6 caracteres';
+        }if($this->password !== $this->password2){
+            self::$alertas['error'][] = 'El password no es igual';
+        }
+
+
+
+
+        return self::$alertas;
+    }
+}
