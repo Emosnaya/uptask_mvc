0ab84aed0e3c26717fe68e129fd16edda425af86
Emmanuel Osnaya <92100726+Emosnaya@users.noreply.github.com>
2022-06-23 23:32:28 -0500

    Finalizamos funcionalidad de las tareas

--- views/dashboard/proyecto.php
+++ views/dashboard/proyecto.php
@@ -5,6 +5,25 @@
             <button type="button" class="agregar-tarea" id="agregar-tarea">&#43; Nueva Tarea</button>
         </div>
 
+        <div id="filtros" class="filtros">
+            <div class="filtros-inputs">
+                <h2>Filtros: </h2>
+                <div class="campo">
+                    <label for="todas">Todas</label>
+                    <input type="radio" name="filtro" id="todas" value="" checked>
+                </div>
+                <div class="campo">
+                    <label for="completadas">Completadas</label>
+                    <input type="radio" name="filtro" id="completadas" value="1">
+                </div>
+                <div class="campo">
+                    <label for="pendientes">Pendientes</label>
+                    <input type="radio" name="filtro" id="pendientes" value="2">
+                </div>
+
+            </div>
+        </div>
+
         <ul id="listado-tareas" class="listado-tareas"></ul>
     </div>
 
@@ -13,6 +32,7 @@
 
 <?php 
 $script = '
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="/build/js/tareas.js"></script>
 ';
 ?>
--- views/layout.php
+++ views/layout.php
@@ -6,7 +6,8 @@
     <title>UpTask | <?php echo $titulo ?? ''; ?></title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;900&family=Open+Sans&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;900&family=Open+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css"> 
     <link rel="stylesheet" href="build/css/app.css">
 </head>
 <body>
